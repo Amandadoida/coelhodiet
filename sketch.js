3906c93e3c6724909266ba1c0a84824185b9fd27
Amandadoida <94861251+Amandadoida@users.noreply.github.com>
2022-04-08 14:49:25 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,12 +17,26 @@ var nutricao;
 var alfredo;
 var alfredofitness;
 var tentacao;
+var piscadinha;
+var seAlimentando;
+var comeuBesteira;
 
 function preload(){
   academia = loadImage("background.png");
   nutricao = loadImage("melon.png");
   alfredo = loadImage("Rabbit-01.png");
 
+  piscadinha = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  seAlimentando = loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+comeuBesteira = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+
+
+  piscadinha.playing = true;
+  seAlimentando.playing = true;
+  comeuBesteira.playing = true;
+
+  seAlimentando.looping = false;
+comeuBesteira.looping = false;
 }
 
 function setup() 
@@ -30,9 +44,12 @@ function setup()
   createCanvas(500,700);
   engine = Engine.create();
   world = engine.world;
+
+  piscadinha.frameDelay = 20;
+  seAlimentando.frameDelay = 20;
  
   ground = new Ground(200, 690, 600, 20);
-linguica=new Rope(6,{x:245,y:30});
+  linguica=new Rope(6,{x:245,y:30});
 
 var options={
   density:0.001
@@ -44,7 +61,11 @@ grandechurrasco=new Grandechurrasco(linguica,natura)
 
 alfredofitness=createSprite(250,630,100,100);
 alfredofitness.addImage(alfredo);
-alfredofitness.scale=0.3;
+alfredofitness.scale=0.2;
+alfredofitness.addAnimation("piscadinha", piscadinha);
+alfredofitness.addAnimation("seAlimentando", seAlimentando);
+alfredofitness.addAnimation("comeuBesteira",comeuBesteira);
+alfredofitness.changeAnimation("piscadinha");
 
 tentacao=createImg("cut_button.png");
 tentacao.position(220,30);
@@ -64,12 +85,23 @@ function draw()
   image(academia, width/2, height/2, 500, 700);
 
   Engine.update(engine);
-  image(nutricao, natura.position.x,natura.position.y,60,60);
+  if(natura!==null){
+    image(nutricao, natura.position.x,natura.position.y,60,60);
+  
+  }
+  
   
 
 
   ground.show();
   linguica.show();
+  if( alfredoencontroufelicidade(natura,alfredofitness)===true){
+    alfredofitness.changeAnimation("seAlimentando");
+  }
+  if(natura!==null&&natura.position.y>=650){
+    alfredofitness.changeAnimation("comeuBesteira");
+    natura=null;
+  }
   
   drawSprites();
 
@@ -81,5 +113,16 @@ grandechurrasco=null;
 
 }
 
-
+function alfredoencontroufelicidade(corpo,sprite){
+  if(corpo!==null){
+    var longedanutricionista= dist(corpo.position.x,corpo.position.y,sprite.position.x,sprite.position.y);
+    if(longedanutricionista<=80){
+      World.remove(engine.world,natura);
+      natura=null;
+      return true;
+    } else{
+      return false;
+    }
+  }
+}
 
